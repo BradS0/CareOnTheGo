8283b0495191c8ee09dadbc4fb19268cf57c1142
BradS0 <bradley.onyett@gmail.com>
2020-04-06 20:15:21 +0100

    - Completed database set-up (Now ready to implement actual data) - Created textView's and appropriate buttons for the patient information page - Added relevant information for Patient Information page design into strings.xml

--- app/src/main/java/com/example/careonthego/DatabaseHelper.java
+++ app/src/main/java/com/example/careonthego/DatabaseHelper.java
@@ -10,6 +10,8 @@ import androidx.annotation.RequiresApi;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "CareOnTheGo.db";
+    public static final Integer DATABASE_VERSION = 1;
+
     public static final String USER_COL1 = "ID"; //PK
     public static final String USERFEEDBACK_COL2 = "USERID"; //FK
     public static final String USERINFO_COL2 = "FIRSTNAME";
@@ -45,12 +47,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String PATIENTMED_COL4 = "MEDICATIONNOTES";
 
     public static final String TABLE_PATIENTINFO = "patientInfo_table";
+    public static final String PATIENTINFO_COL1 = "PATIENTINFOID";
     public static final String PATIENTINFO_COL5 = "AGE";
     public static final String PATIENTINFO_COL6 = "ADDRESS";
     public static final String PATIENTINFO_COL7 = "RELEVANTNOTES";
     public static final String PATIENTINFO_COL8 = "EMERGENCYNUMBERS";
 
-    public static final String TABLE_MEDICATION_ = "medication_table";
+    public static final String TABLE_MEDICATION = "medication_table";
     public static final String MEDICATION_COL2 = "MEDICATIONNAME";
     public static final String MEDICATION_COL3 = "MEDICATIONQUANTITY";
 
@@ -68,6 +71,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void onCreate(SQLiteDatabase db) {
+
         db.execSQL("create table " + TABLE_USER + "("+USER_COL1+" INTEGER PRIMARY KEY AUTOINCREMENT,"+USER_COL2+" TEXT, "+USER_COL3+" TEXT)");
         db.execSQL("create table " + TABLE_USERINFO + "("+USER_COL1+" INTEGER PRIMARY KEY AUTOINCREMENT,"+USERINFO_COL2+" TEXT, "+USERINFO_COL3+" TEXT, "+USERINFO_COL4+" INT, "+USERINFO_COL5+"TEXT)");
 
@@ -77,12 +81,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("create table " + TABLE_TASK + "("+TASK_COL1+" INTEGER PRIMARY KEY AUTOINCREMENT,"+USERFEEDBACK_COL2+" INTEGER, "+TASK_COL3+" TEXT, "+TASK_COL4+" TEXT, "+TASK_COL5+"TEXT, "+TASK_COL6+"TEXT, FOREIGN KEY ("+USERFEEDBACK_COL2+") REFERENCES "+TABLE_USER+" ("+USER_COL1+"))");
         db.execSQL("create table " + TABLE_PATIENTMED + "("+PATIENTMED_COL1+" INTEGER PRIMARY KEY AUTOINCREMENT,"+PATIENTMED_COL2+" INTEGER , "+PATIENTMED_COL3+" INTEGER, "+PATIENTMED_COL4+" TEXT)");
         db.execSQL("create table " + TABLE_PATIENTINFO + "("+USER_COL1+" INTEGER PRIMARY KEY AUTOINCREMENT,"+USERFEEDBACK_COL2+" INTEGER, "+USERINFO_COL2+" TEXT, "+USERINFO_COL3+" TEXT, "+PATIENTINFO_COL5+"INTEGER, "+PATIENTINFO_COL6+"TEXT, "+PATIENTINFO_COL7+"TEXT, "+PATIENTINFO_COL8+"TEXT, FOREIGN KEY ("+USERFEEDBACK_COL2+") REFERENCES "+TABLE_USER+" ("+USER_COL1+"))");
-       // db.execSQL("create table " + TABLE_MEDICATION_ + "("+PATIENTMED_COL3+" INTEGER PRIMARY KEY AUTOINCREMENT,"+MEDICATION_COL2+" TEXT, "+MEDICATION_COL3+" TEXT)");
+        db.execSQL("create table " + TABLE_MEDICATION + "("+PATIENTMED_COL3+" INTEGER PRIMARY KEY AUTOINCREMENT,"+MEDICATION_COL2+" TEXT, "+MEDICATION_COL3+" INTEGER)");
+
 
         String USER_TABLE_CREATE = "CREATE TABLE " + TABLE_USER + " ("
                 + USER_COL1 + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + USER_COL2 + " TEXT, "
-                + USER_COL3 + " TEXT, );";
+                + USER_COL3 + " TEXT);";
         db.execSQL(USER_TABLE_CREATE);
 
         String USERINFO_TABLE_CREATE = "CREATE TABLE " + TABLE_USERINFO + " ("
@@ -98,20 +103,53 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 + USERFEEDBACK_COL2 + " INTEGER, "
                 + USERFEEDBACK_COL3 + " INTEGER, "
                 + USERFEEDBACK_COL4 + " TEXT, "
-                + USERFEEDBACK_COL5 + " TEXT FOREIGN KEY("+USERFEEDBACK_COL2+") REFERENCES "+TABLE_USER+"("+USER_COL1+"));";
-        db.execSQL(USERINFO_TABLE_CREATE);
+                + USERFEEDBACK_COL5 + " TEXT, FOREIGN KEY("+USERFEEDBACK_COL2+") REFERENCES "+TABLE_USER+"("+USER_COL1+"));";
+        db.execSQL(USERFEEDBACK_TABLE_CREATE);
 
+        String TASK_TABLE_CREATE = "CREATE TABLE " + TABLE_TASK + " ("
+                + TASK_COL1 + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + USERFEEDBACK_COL2 + " TEXT, "
+                + TASK_COL3 + " TEXT, "
+                + TASK_COL4 + " TEXT, "
+                + TASK_COL5 + " TEXT, "
+                + TASK_COL6 + " TEXT, FOREIGN KEY("+USERFEEDBACK_COL2+") REFERENCES "+TABLE_USER+"("+USER_COL1+"));";
+        db.execSQL(TASK_TABLE_CREATE);
 
+        String PATIENTMED_TABLE_CREATE = "CREATE TABLE " + TABLE_PATIENTMED + " ("
+                + PATIENTMED_COL1 + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + PATIENTMED_COL2 + " INTEGER, "
+                + PATIENTMED_COL3 + " INTEGER, "
+                + PATIENTMED_COL4 + " TEXT);"; // don't forget to make this with two FK's.
+        db.execSQL(PATIENTMED_TABLE_CREATE);
 
+        String PATIENTINFO_TABLE_CREATE = "CREATE TABLE " + TABLE_PATIENTINFO + " ("
+                + PATIENTINFO_COL1 + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + USERFEEDBACK_COL2 + " INTEGER, "
+                + USERINFO_COL2 + " TEXT, "
+                + USERINFO_COL3 + " TEXT, "
+                + PATIENTINFO_COL5 + " INTEGER, "
+                + PATIENTINFO_COL6 + " TEXT, "
+                + PATIENTINFO_COL7 + " TEXT, "
+                + PATIENTINFO_COL8 + " TEXT, FOREIGN KEY("+USERFEEDBACK_COL2+") REFERENCES "+TABLE_USER+"("+USER_COL1+"));";
+        db.execSQL(PATIENTINFO_TABLE_CREATE);
+
+        String MEDICATION_TABLE_CREATE = "CREATE TABLE " + TABLE_MEDICATION + " ("
+                + PATIENTMED_COL3 + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + MEDICATION_COL2 + " TEXT, "
+                + MEDICATION_COL3 + " INTEGER);";
+        db.execSQL(MEDICATION_TABLE_CREATE);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
-        db.execSQL("DELETE FROM " + TABLE_USER);
-        //db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERINFO);
-        //db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERFEEDBACK);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERINFO);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERFEEDBACK);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASK);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PATIENTMED);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PATIENTINFO);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MEDICATION);
         onCreate(db);
         db.close();
     }
